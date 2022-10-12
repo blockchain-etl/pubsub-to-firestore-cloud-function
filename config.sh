ee9b6e89b050a7c56f307fe1150bece5375497e5
Allen Day <allenday@users.noreply.github.com>
2022-10-12 16:51:06 +0800

    bump version

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 FUNCTION_NAME=your-function-name
-FUNCTION_RUNTIME=nodejs8
+FUNCTION_RUNTIME=nodejs12
 FUNCTION_TRIGGER_TOPIC=your_pubsub_topic
 FIRESTORE_COLLECTION_NAME=your_firestore_collection_name
